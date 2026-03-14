69bc291bb3eeeda24607bad804ad44ed9570b98f
Romat1981 <mattosbm6@gmail.com>
2026-03-14 07:21:27 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,54 +1,52 @@
 var produtos = [
 
-
-
 {
 nome:"iPhone 14 Pro Max 128GB",
 preco:"R$ 5.339",
-img:"https://http2.mlstatic.com/D_NQ_NP_2X_896507-MLA96123118445_102025-F.webp",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_896507-MLA96123118445_102025-F.webp",
 link:"https://meli.la/2a6ue9k"
 },
 
 {
 nome:"Carregador Turbo 40W iPhone",
 preco:"R$ 43,55",
-img:"https://http2.mlstatic.com/D_NQ_NP_2X_609505-MLA105673814081_012026-F.webp",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_609505-MLA105673814081_012026-F.webp",
 link:"https://meli.la/2HUZgxD"
 },
 
 {
-nome:"Sound Bar JBL Cinema SB180",
+nome:"Barra de som JBL Cinema SB180",
 preco:"R$ 925",
-img:"https://http2.mlstatic.com/D_NQ_NP_2X_936543-MLA99989050845_112025-F.webp",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_936543-MLA99989050845_112025-F.webp",
 link:"https://meli.la/31uqXbU"
 },
 
 {
 nome:"Creatina Growth 250g",
 preco:"R$ 39,90",
-img:"https://http2.mlstatic.com/D_NQ_NP_2X_662415-MLA97812910758_112025-F.webp",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_662415-MLA97812910758_112025-F.webp",
 link:"https://meli.la/2fvRduS"
 },
 
 {
-nome:"Mangueira Jardim 30m",
+nome:"Mangueira de Jardim 30m",
 preco:"R$ 46,90",
-img:"https://http2.mlstatic.com/D_NQ_NP_2X_988510-MLA99501045164_112025-F.webp",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_988510-MLA99501045164_112025-F.webp",
 link:"https://meli.la/2NnapD8"
 },
 
 {
 nome:"Amaciante Comfort Pro 7L",
 preco:"R$ 102,59",
-img:"https://http2.mlstatic.com/D_NQ_NP_2X_779417-MLA107178849021_022026-F.webp",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_779417-MLA107178849021_022026-F.webp",
 link:"https://meli.la/2zFHux2"
 },
 
 {
 nome:"Panela de Pressão Elétrica Electrolux 6L",
 preco:"R$ 545",
-img:"https://http2.mlstatic.com/D_NQ_NP_2X_661078-MLA99491593886_112025-F.webp",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_661078-MLA99491593886_112025-F.webp",
 link:"https://meli.la/2UVoyHn"
 }
 
-]
+];
