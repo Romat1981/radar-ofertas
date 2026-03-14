801ecf81c0d5db426aa40215d5af8a3523bf936c
Romat1981 <mattosbm6@gmail.com>
2026-03-14 06:26:43 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,51 @@
+const produtos = [
+
+{
+nome:"iPhone 14 Pro Max",
+preco:"R$ 5.339",
+img:"https://http2.mlstatic.com/D_NQ_NP_2X_896507-MLA96123118445_102025-F.webp",
+link:"https://meli.la/2a6ue9k",
+categoria:"eletronicos"
+},
+
+{
+nome:"Carregador Turbo iPhone",
+preco:"R$ 43,55",
+img:"https://http2.mlstatic.com/D_NQ_NP_2X_609505-MLA105673814081_012026-F.webp",
+link:"https://meli.la/2HUZgxD",
+categoria:"eletronicos"
+},
+
+{
+nome:"Sound Bar JBL Cinema",
+preco:"R$ 925",
+img:"https://http2.mlstatic.com/D_NQ_NP_2X_936543-MLA99989050845_112025-F.webp",
+link:"https://meli.la/31uqXbU",
+categoria:"eletronicos"
+},
+
+{
+nome:"Creatina Growth 250g",
+preco:"R$ 39,90",
+img:"https://http2.mlstatic.com/D_NQ_NP_2X_662415-MLA97812910758_112025-F.webp",
+link:"https://meli.la/2fvRduS",
+categoria:"suplementos"
+},
+
+{
+nome:"Mangueira Jardim 30m",
+preco:"R$ 46,90",
+img:"https://http2.mlstatic.com/D_NQ_NP_2X_988510-MLA99501045164_112025-F.webp",
+link:"https://meli.la/2NnapD8",
+categoria:"casa"
+},
+
+{
+nome:"Panela de Pressão Elétrica Electrolux",
+preco:"R$ 545",
+img:"https://http2.mlstatic.com/D_NQ_NP_2X_661078-MLA99491593886_112025-F.webp",
+link:"https://meli.la/2UVoyHn",
+categoria:"casa"
+}
+
+];
