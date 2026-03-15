d575241617396906370004c7816196c5db290161
Romat1981 <mattosbm6@gmail.com>
2026-03-15 17:43:33 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,24 +1,171 @@
 var produtos = [
 
 {
+nome:"Smartphone Motorola Moto G06 128GB",
+preco:"R$ 599,40",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_945623-MLA96724963315_102025-F.webp",
+link:"https://meli.la/2WVQXUt"
+},
+
+{
+nome:"Air Fryer Mondial 4L 1500W",
+preco:"R$ 269,10",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_775407-MLA99776749612_122025-F.webp",
+link:"https://meli.la/19LrGhn"
+},
+
+{
+nome:"Smart TV Samsung 32 HD",
+preco:"R$ 949,90",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_959706-MLA99950013507_112025-F.webp",
+link:"https://meli.la/1YM957K"
+},
+
+{
+nome:"Smart TV AOC 50 4K Roku",
+preco:"R$ 2.193,87",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_648316-MLA99473453820_112025-F.webp",
+link:"https://meli.la/2HLz2ko"
+},
+
+{
+nome:"Kit Perfumes Cebolinha Jequiti",
+preco:"R$ 65,85",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_747644-MLA79343378494_092024-F.webp",
+link:"https://meli.la/1r3WA71"
+},
+
+{
+nome:"Body Splash Barbarius 200ml",
+preco:"R$ 86,90",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_847667-MLA97122485144_112025-F.webp",
+link:"https://meli.la/2Yr5NNa"
+},
+
+{
+nome:"Playstation 5 Slim + Gran Turismo 7",
+preco:"R$ 3.822,29",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_945456-MLA99456386724_112025-F.webp",
+link:"https://meli.la/2FcoRG5"
+},
+
+{
+nome:"Kit 2 Câmeras Segurança Wifi",
+preco:"R$ 185,99",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_901090-MLA99414404486_112025-F.webp",
+link:"https://meli.la/1kyaCgG"
+},
+
+{
+nome:"Parafusadeira Furadeira Impacto",
+preco:"R$ 179,00",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_865332-MLA96868279679_102025-F.webp",
+link:"https://meli.la/21ngykQ"
+},
+
+{
+nome:"Chuveiro Lorenzetti Acqua Ultra Duo",
+preco:"R$ 505,10",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_661398-MLA83775380474_042025-F.webp",
+link:"https://meli.la/26DdA1c"
+},
+
+{
+nome:"Chapinha Lizze 480 Extreme",
+preco:"R$ 413,10",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_999316-MLA95680388786_102025-F.webp",
+link:"https://meli.la/2Apimbe"
+},
+
+{
+nome:"Perfume Attracione Men",
+preco:"R$ 66,93",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_948198-MLA81769955450_012025-F.webp",
+link:"https://meli.la/1JYx8Ef"
+},
+
+{
+nome:"Cicaplast Baume B5 La Roche",
+preco:"R$ 65,11",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_653286-MLA88348742037_072025-F.webp",
+link:"https://meli.la/2x2CfS5"
+},
+
+{
+nome:"Protetor Solar Anthelios Airlicium",
+preco:"R$ 66,45",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_964404-MLA99926127671_112025-F.webp",
+link:"https://meli.la/2aZvKjD"
+},
+
+{
+nome:"Suporte 2 Controles Xbox",
+preco:"R$ 30,09",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_959146-MLB92886306385_092025-F.webp",
+link:"https://meli.la/2i6ub5S"
+},
+
+{
+nome:"Skin PS5 Slim Preta",
+preco:"R$ 34,90",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_862458-MLB92654234481_092025-F.webp",
+link:"https://meli.la/1rBM53X"
+},
+
+{
+nome:"Volante Logitech G29 Driving Force",
+preco:"R$ 1.799,00",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_730363-MLA99890032125_112025-F.webp",
+link:"https://meli.la/1yxcfGM"
+},
+
+{
 nome:"iPhone 14 Pro Max 128GB",
-preco:"R$ 5.339",
+preco:"R$ 5.339,99",
 foto:"https://http2.mlstatic.com/D_NQ_NP_2X_896507-MLA96123118445_102025-F.webp",
 link:"https://meli.la/2a6ue9k"
 },
 
 {
-nome:"Carregador Turbo 40W iPhone",
+nome:"Carregador Turbo Duplo 40W",
 preco:"R$ 43,55",
 foto:"https://http2.mlstatic.com/D_NQ_NP_2X_609505-MLA105673814081_012026-F.webp",
 link:"https://meli.la/2HUZgxD"
 },
 
 {
-nome:"Barra de som JBL Cinema SB180",
-preco:"R$ 925",
+nome:"Sound Bar JBL SB180",
+preco:"R$ 925,00",
 foto:"https://http2.mlstatic.com/D_NQ_NP_2X_936543-MLA99989050845_112025-F.webp",
 link:"https://meli.la/31uqXbU"
+},
+
+{
+nome:"Creatina Growth 250g",
+preco:"R$ 39,90",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_662415-MLA97812910758_112025-F.webp",
+link:"https://meli.la/2fvRduS"
+},
+
+{
+nome:"Mangueira Jardim 30m",
+preco:"R$ 46,90",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_988510-MLA99501045164_112025-F.webp",
+link:"https://meli.la/2NnapD8"
+},
+
+{
+nome:"Amaciante Comfort Pro 7L",
+preco:"R$ 102,59",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_779417-MLA107178849021_022026-F.webp",
+link:"https://meli.la/2zFHux2"
+},
+
+{
+nome:"Panela de Pressão Elétrica Electrolux",
+preco:"R$ 545,10",
+foto:"https://http2.mlstatic.com/D_NQ_NP_2X_661078-MLA99491593886_112025-F.webp",
+link:"https://meli.la/2UVoyHn"
 }
 
 ];
